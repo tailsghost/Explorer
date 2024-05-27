441356ce2b654e41c5639fe3f80c2df891fc9e37
AndreyXP <andreyapple99@mail.ru>
2024-05-28 02:02:02 +0300

    Подготовка проекта

---
+++ Explorer.Avalonia.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Explorer.Shared.ViewModels;
+
+namespace Explorer.Avalonia.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- WPF/Explorer.WPF.UI/App.xaml.cs
+++ WPF/Explorer.WPF.UI/App.xaml.cs
@@ -1,14 +1,15 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Explorer.WPF.UI
+namespace Explorer.WPF.UI;
+
+public partial class App
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    protected override void OnStartup(StartupEventArgs e)
     {
-    }
+        base.OnStartup(e);
 
+        var mainWindow = new MainWindow();
+
+        mainWindow.Show();
+    }
 }
